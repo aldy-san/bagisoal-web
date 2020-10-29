fd934b564a5c6c9b9ac5647c24717c60f3fa68f6
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2020-10-29 20:29:18 +0700

    Create tulis-pertanyaan.php

---
+++ tulis-pertanyaan.php
@@ -0,0 +1,175 @@
+<?php 
+require 'functions.php'; 
+if (isset($_POST["submit"])) {
+  if(addpertanyaan($_POST)>0) {
+    echo "<script>
+            alert('Data Berhasil Ditambahkan');
+          </script>
+    ";
+  }else{
+    echo "<script>
+            alert('Data Gagal Ditambahkan');
+          </script>
+    ";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/91e0a5b445.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="res/bs.png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>BAGISOAL</title>
+  </head>
+  <body>
+    <!-- NAVIGATION -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+      <a class="navbar-brand ml-2" style="font-weight: bold;">BAGI<span class="text-warning">SOAL</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+       <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">BERANDA<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="soal-main.html">SOAL-SOAL</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="kompetisi-main.html">KOMPETISI</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="komunitas.html">KOMUNITAS</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              PERINGKAT
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">PROVINSI</a>
+              <a class="dropdown-item" href="#">KOTA</a>
+              <a class="dropdown-item" href="#">SEKOLAH</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#help">BANTUAN</a>
+          </li>
+        </ul>
+        <form class="form-inline mx-auto my-2">
+          <div class="input-group">
+            <input class="form-control mr-2 input" type="search" placeholder="Cari" aria-label="Search">
+            <div class="input-group-append">
+              <button class="btn btn-warning d-inline-block" type="submit"><i class="fas fa-search"></i></button>
+            </div>
+          </div>
+        </form>
+        <div class="mr-2 ml-0">
+          <a class="btn btn-outline-warning mx-1" href="login.html">MASUK/DAFTAR</a>
+        </div>
+      </div>
+    </nav>
+      <!-- CONTENT -->
+      <div class="container text-center p-3 my-3 rounded bg-white shadow">
+        <div class="row px-5">
+          <div class="col-12">
+            <h1>Tulis Pertanyaan Anda</h1>
+          </div>
+        </div>
+        <div class="row my-2 px-5">
+          <div class="col-12">
+            <div class="container text-left">
+              <form method="post" action="">
+                <div class="form-group">
+                  <label for="title"><b class="h4">Judul Pertanyaan</b></label>
+                  <input class="form-control" type="text" name="judul_pertanyaan" id="title" placeholder="Judul Pertanyaan">
+                </div>
+                <div class="form-group">
+                  <label for="Jelaskan"><b class="h4">Deskripsi Pertanyaan</b></label>
+                  <textarea name="isi_pertanyaan" class="form-control" rows="7" placeholder="Jelaskan Pertanyaan"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="tag"><b class="h4">Tags</b></label>
+                  <input name="tags_pertanyaan" type="text" class="form-control" rows="10" placeholder="Tags: #SBMPTN #UTBK"></input>
+                </div>
+                <input type="submit" class="btn btn-primary" value="Posting Pertanyaan" name="submit">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- FOOTER -->
+      <div id="help" class="container-fluid p-md-3 mt-5 text-center rounded shadow-lg">
+        <div class="row mx-md-5 px-md-5 py-3">
+          <div class="col-12 col-md-4 w-25 mb-4">
+            <h5 class="mb-3">Tentang</h5>
+            <p class="text-justify">
+              Website ini berfungsi untuk memberikan layanan media informasi kepada  siswa-siswi Indonesia khususnya SMA yang kesulitan untuk mencari latihan soal.
+            </p>
+          </div>
+          <div class="col-12 col-md-4 w-25 mb-4">
+            <h5 class="mb-3">Sosial Media</h5>
+            <ul class="list-group list-group-horizontal w-auto mx-auto">
+              <li class="list-group-item border-0 mx-auto">
+                <a href="" class="text-dark">
+                  <i class="fab fa-facebook fa-2x my-2"></i>
+                </a>
+              </li>
+              <li class="list-group-item border-0 mx-auto">
+                <a href="" class="text-dark">
+                  <i class="fab fa-twitter fa-2x my-2 "></i>
+                </a>
+              </li>
+              <li class="list-group-item border-0 mx-auto">
+                <a href="" class="text-dark">
+                  <i class="fab fa-instagram fa-2x my-2"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-12 col-md-4 w-25 mb-4">
+            <h5 class="mb-3">Kontak</h5>
+            <ul class="list-group text-left">
+              <li class="list-group-item border-0">
+                <a href="" style="text-decoration: none;" class="text-dark">
+                  <i class="fas fa-envelope fa-lg d-inline mr-2"></i>
+                  <p class="h6 d-inline align-items-center">bagisoal@gmail.com</p>
+                </a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="" style="text-decoration: none;" class="text-dark">
+                  <i class="fas fa-phone fa-lg d-inline mr-2"></i>
+                  <p class="h6 d-inline align-items-center">0877-1234-1234 (Moshi)</p>
+                </a>
+              </li>
+              <li class="list-group-item border-0">
+                <a href="" style="text-decoration: none;" class="text-dark">
+                  <i class="fas fa-map-marker-alt fa-lg d-inline mr-2"></i>
+                  <p class="h6 d-inline align-items-center">Indonesia</p>
+                </a>
+              </li>
+            </ul>
+          </div> 
+        </div>
+        <div class="row">
+          <div class="col-12 mt-3">
+            Copyright © 2020 BAGISOAL
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" "></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>
+  </body>
+</html>
